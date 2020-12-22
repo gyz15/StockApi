77141f5eabf30b1d1ed7cbf94fb189be7d9cf294
Gnaw Yuan Zhen <yzgnaw@gmail.com>
2020-12-22 08:22:00 +0800

    Get fundamental data

--- StockApi.py
+++ StockApi.py
@@ -4,4 +4,23 @@ import requests
 load_dotenv()
 
 API_KEY = os.getenv('ALPHAVANTAGE')
-print(API_KEY)
+
+CHECK_TYPE = (
+    "OVERVIEW",
+    "INCOME_STATEMENT",
+    "BALANCE_SHEET",
+    "CASH_FLOW",
+    "EARNINGS",
+    "LISTING_STATUS",
+)
+
+
+def get_stock(name, check_type, api_key):
+    data = requests.get(
+        f"https://www.alphavantage.co/query?function={check_type}&symbol={name}&apikey={api_key}")
+    return data.json()
+
+
+stock_data = get_stock("TSLA", CHECK_TYPE[0], API_KEY)
+print(stock_data)
+print(type(stock_data))
